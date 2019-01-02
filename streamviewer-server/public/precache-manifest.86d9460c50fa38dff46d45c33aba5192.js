4e9aeae1400db00b86fd9efb66f57212372724cd
dharanikumarp <dharani.kumar@gmail.com>
2019-01-02 18:33:24 -0500

    Issue #6 - Test deployment in local setup

---
+++ streamviewer-server/public/precache-manifest.86d9460c50fa38dff46d45c33aba5192.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "db369fa20c5242c215ae",
+    "url": "/static/js/main.db369fa2.chunk.js"
+  },
+  {
+    "revision": "64778e929725b40fd6cd",
+    "url": "/static/js/1.64778e92.chunk.js"
+  },
+  {
+    "revision": "db369fa20c5242c215ae",
+    "url": "/static/css/main.ce3feb13.chunk.css"
+  },
+  {
+    "revision": "64778e929725b40fd6cd",
+    "url": "/static/css/1.b5527b30.chunk.css"
+  },
+  {
+    "revision": "08ad43d2dbc6c033080a100d68e05688",
+    "url": "/index.html"
+  }
+];
