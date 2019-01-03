bde76c4164f43fe30af6f99050ea971b6f7044ca
dharanikumarp <dharani.kumar@gmail.com>
2019-01-02 23:52:24 -0500

    Issue #6 - Add heroku server URL to the constants

--- streamviewer-client/src/Urls.js
+++ streamviewer-client/src/Urls.js
@@ -1,6 +1,13 @@
-export const LOGIN = 'http://localhost:9000/login';
-export const STREAMS ='http://localhost:9000/streams';
-export const MESSAGES = 'http://localhost:9000/messages';
-export const USERS = 'http://localhost:9000/users';
-export const WS_CHAT = 'ws://localhost:9000/chat';
-export const WS_STATS = 'ws://localhost:9000/stats';
+// export const LOGIN = 'http://localhost:9000/login';
+// export const STREAMS ='http://localhost:9000/streams';
+// export const MESSAGES = 'http://localhost:9000/messages';
+// export const USERS = 'http://localhost:9000/users';
+// export const WS_CHAT = 'ws://localhost:9000/chat';
+// export const WS_STATS = 'ws://localhost:9000/stats';
+
+export const LOGIN = 'https://dharani-sl-assignment.herokuapp.com/login';
+export const STREAMS ='https://dharani-sl-assignment.herokuapp.com/streams';
+export const MESSAGES = 'https://dharani-sl-assignment.herokuapp.com/messages';
+export const USERS = 'https://dharani-sl-assignment.herokuapp.com/users';
+export const WS_CHAT = 'wss://dharani-sl-assignment.herokuapp.com/chat';
+export const WS_STATS = 'wss://dharani-sl-assignment.herokuapp.com/stats';
